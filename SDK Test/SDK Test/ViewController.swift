caf5f4532acc60d7204d52105ff0a7b7e97d0518
Taylor Whitlatch <taylorwhitlatch@Taylors-MacBook-Air-2.local>
2019-07-19 17:40:52 -0400

    initial

---
+++ SDK Test/SDK Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDK Test
+//
+//  Created by Taylor Whitlatch on 7/19/19.
+//  Copyright © 2019 Taylor Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
